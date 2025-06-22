e4879e3fa74c21b175218194bf9956c0460b0378
Ana Alexandru-Gabriel <ana.alexandru.gabriel@proton.me>
2025-06-22 16:03:11 +0300

    added variable access to url parameters
    Signed-off-by: Ana Alexandru-Gabriel <ana.alexandru.gabriel@proton.me>

--- README.md
+++ README.md
@@ -28,3 +28,27 @@ int main() {
   ptth_start(server);
 }
 ```
+You're endpoints don't have to be static, you can retrieve name segments of your URL and retrieve them at run time. Here is an example where when you access /hi/<name>/ it will display the message "Hi, <name>! :3".
+```c
+#include "ptt.h"
+#include <unistd.h>
+
+ptth_response_t get_func(ptth_request_t req, ptth_shared_t *__) {
+  char *test = ptth_map_get_value(*(req.mpath), "var_name");
+  char message[100];
+  message[0] = '\0';
+  strcat(message, "Hi, ");
+  strcat(message, test);
+  strcat(message, "! :3");
+  PTTH_RESPONDE_HTML(message)
+}
+
+int main() {
+  ptth_server_t server;
+  ptth_init_server(&server);
+  ptth_add_service(&server, PTTH_GET, "/hi/{var_name}/", &get_func);
+  ptth_bind_server(&server, "127.0.0.1", 8081);
+  ptth_start(server);
+}
+```
+![example_hi](./plus/hi_alex.png)
--- ptt.h
+++ ptt.h
@@ -1,3 +1,11 @@
+/* The original source code of this library is licensed under th GPLv3 License.
+ * So have fun with this :3.
+ *
+ * Original Maintainer: Ana Alexandru-Gabriel <ana.alexandru.gabriel@proton.me>
+ *
+ * */
+
+
 #ifndef ___PTT_H___
 #define ___PTT_H___
 #include <pthread.h>
@@ -8,9 +16,15 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <arpa/inet.h>
+#include <regex.h>
 
 #define PTTH_MAX_HEADER_SIZE 4096
-#define REQUEST_BUFFER_SIZE 10000
+#define PTTH_REQ_BUFFER_SIZE 10000
+#define PTTH_SIZE_OF_MAP 10
+
+
+#define PTTH1_1 "HTTP/1.1"
+#define PTTH1_1_404_NOT_FOUND PTTH1_1" 404 Not Found"
 
 enum ptth_methods_e { 
   PTTH_GET    = 0,
@@ -19,6 +33,12 @@ enum ptth_methods_e {
   PTTH_DELETE = 3,
   PTTH_PATCH  = 4
 };
+
+typedef struct ptth_map_t {
+  char *keys[PTTH_SIZE_OF_MAP];
+  char *vals[PTTH_SIZE_OF_MAP];
+  size_t len;
+} ptth_map_t;
 
 typedef struct ptth_header_t {
   char *head;
@@ -30,6 +50,7 @@ typedef struct ptth_body_t {
 
 typedef struct ptth_path_t {
   char *path;
+  ptth_map_t kv_map;
 } ptth_path_t;
 
 typedef struct ptth_shared_t {
@@ -38,7 +59,7 @@ typedef struct ptth_shared_t {
 } ptth_shared_t;
 
 typedef struct ptth_request_t {
-  ptth_path_t      path;
+  ptth_map_t     *mpath;
   ptth_header_t headers;
   ptth_body_t      body;
 } ptth_request_t;
@@ -48,9 +69,15 @@ typedef struct ptth_response_t {
   ptth_body_t      body;
 } ptth_response_t;
 
+typedef struct ptth_endpoint_t {
+  regex_t rgx; 
+} ptth_endpoint_t;
+
 typedef struct ptth_service_t {
   struct ptth_service_t                              *next;
-  char                                           *endpoint;
+  regex_t                                        *endpoint;
+  ptth_map_t                                      *url_map;
+  int                              order[PTTH_SIZE_OF_MAP];
   ptth_response_t (*sproc)(ptth_request_t, ptth_shared_t*);
 } ptth_service_t;
 
@@ -72,6 +99,10 @@ void ptth_init_server(ptth_server_t *);
 void ptth_add_service(ptth_server_t *, enum ptth_methods_e, char *, ptth_response_t (*)(ptth_request_t, ptth_shared_t*));
 void ptth_services_new_service(ptth_services_t *service, char *endpoint, ptth_response_t (*)(ptth_request_t, ptth_shared_t*));
 void ptth_bind_server(ptth_server_t *, char *, size_t);
+int ptth_map_add_kv(ptth_map_t *, char *, char *);
+ptth_response_t ptth_new_repsonse();
+ptth_request_t ptth_empty_request();
+
 #define PTTH_RESPONDE_HTML(response_payload)\
   ptth_response_t resp;\
   resp.body.body=malloc(strlen((response_payload)) + 1);\
@@ -80,11 +111,117 @@ void ptth_bind_server(ptth_server_t *, char *, size_t);
   strcpy(resp.headers.head, "HTTP/1.1 202 Accepted\r\n""Content-Type: text/html\r\n""Connection: close\r\n""\r\n");\
   return resp;
 
+
+char *ptth_map_value(ptth_map_t map, char *key) {
+  if(map.keys == NULL || map.vals == NULL) return NULL;
+  for(size_t i = 0; i < PTTH_SIZE_OF_MAP; i++) {
+    if(map.keys[i] == NULL) break;
+    if(strcmp(map.keys[i], key) == 0) return map.vals[i];
+  }
+  return NULL;
+}
+
+int ptth_key_index(ptth_map_t map, char *fk) {
+  for(size_t i = 0; i < map.len; i++) {
+    if(strcmp(map.keys[i], fk) == 0) return i;
+  }
+  return -1;
+}
+
+char *ptth_map_get_value(ptth_map_t map, char *key) {
+  for(size_t i = 0; i<map.len; i++) {
+    if(strcmp(map.keys[i], key) == 0) return map.vals[i];
+  }
+  return NULL;
+}
+
+ptth_map_t *new_ptth_map() {
+  ptth_map_t * ret = malloc(sizeof(ptth_map_t));
+  ret -> len = 0;
+  return ret;
+}
+
+int ptth_map_set_value(ptth_map_t *map, char *key, char *val) {
+  if(map == NULL) return 0;
+  for(size_t i = 0; i < map -> len; i++) {
+    if(strcmp(map -> keys[i], key) == 0) {
+      if(map -> vals[i] != NULL) free(map -> vals[i]);
+      map -> vals[i] = malloc(strlen(val) + 1);
+      strcpy(map -> vals[i], val);
+      return 1;
+    }
+  }
+  return ptth_map_add_kv(map, key, val);
+}
+
+int ptth_map_add_kv(ptth_map_t *map, char *key, char *val) {
+  if(map == NULL) return 0;
+
+
+  map -> keys[map -> len] = malloc(strlen(key));
+  map -> vals[map -> len] = malloc(strlen(val));
+
+  strcpy(map -> keys[map -> len], key);
+  strcpy(map -> vals[map -> len], val);
+
+  if(map -> len < PTTH_SIZE_OF_MAP - 1) map -> len = map -> len + 1; 
+  return 1;
+}
+
+ptth_response_t ptth_new_reponse() {
+  ptth_response_t *r = malloc(sizeof(ptth_response_t));
+  return *r;
+}
+
+ptth_request_t ptth_empty_request() {
+  ptth_request_t *req = malloc(sizeof(ptth_request_t));
+  return *req;
+}
+
 void ptth_init_server(ptth_server_t *server) {
   for(int i = 0; i<5; i++) 
-    server -> services[i] = NULL;
+    server -> services[i] = malloc(sizeof(ptth_services_t));
   server -> ipaddr = NULL;
   server -> port   =    0;
+}
+
+regex_t *ptth_process_regex(char *load, ptth_map_t *map, int *order) {
+  size_t ll = strlen(load);
+  int exp = 0;
+  char *nload = malloc(20+(strlen(load) * 2));
+  size_t nlc = 0;
+  char nk[20];
+  size_t nkc = 0;
+  size_t sc = -1;
+  for(size_t i = 0; i < ll; i++) {
+    if(load[i] == '/') sc++;
+    if(load[i] == '{') {
+      exp = 1;
+      nload[nlc] = '\0';
+      strcat(nload, "[.a-zA-z0-9]+");
+      nlc = strlen(nload);
+    }
+    if(exp == 0) {
+      nload[nlc] = load[i];
+      nlc++;
+    }
+    if(load[i] == '}') {
+      exp = 0;
+      nk[nkc] = '\0';
+      if(map == NULL)map = new_ptth_map();
+      order[map -> len] = sc;
+      ptth_map_set_value(map, nk, "");
+    }
+    if(exp == 1 && load[i] != '{') {
+      nk[nkc] = load[i];
+      nkc++;
+    }
+  }
+  nload[nlc] = '\0';
+
+  regex_t *nr = malloc(sizeof(regex_t));
+  regcomp(nr, nload, REG_EXTENDED);
+  return nr;
 }
 
 void ptth_services_new_service(
@@ -97,17 +234,19 @@ void ptth_services_new_service(
     service -> head = malloc(sizeof(ptth_service_t));
     service -> head -> next = NULL;
     service -> head -> endpoint = malloc(strlen(endpoint) + 1);
-    strcpy(service -> head -> endpoint, endpoint);
+    service -> head -> url_map = new_ptth_map();
+    service -> head -> endpoint = ptth_process_regex(endpoint, service -> head -> url_map, service -> head -> order);
     service -> head -> sproc = sproc;
     service -> tail = service -> head;
     return;
   }
-
+  
   if(service -> head == service -> tail) {
     service -> tail = malloc(sizeof(ptth_service_t));
     service -> tail -> next = NULL;
     service -> tail -> endpoint = malloc(strlen(endpoint) + 1);
-    strcpy(service -> tail -> endpoint, endpoint);
+    service -> tail -> url_map = new_ptth_map();
+    service -> tail -> endpoint = ptth_process_regex(endpoint, service -> tail -> url_map, service -> tail -> order);
     service -> tail -> sproc = sproc;
     service -> head -> next = service -> tail;
     return;
@@ -117,7 +256,8 @@ void ptth_services_new_service(
   ptth_service_t *new = malloc(sizeof(ptth_service_t));
   new -> next = next;
   new -> endpoint = malloc(strlen(endpoint) + 1);
-  strcpy(new -> endpoint, endpoint);
+  new -> url_map = new_ptth_map();
+  new -> endpoint = ptth_process_regex(endpoint, new -> url_map, new -> order);
   new -> sproc = sproc;
   service -> head -> next = new;
 
@@ -156,11 +296,9 @@ enum ptth_methods_e ptth_get_method(char *buffer) {
   }
   method[i] = '\0';
 
-  printf("%s ", method);
-
   if(strcmp(method, "GET") == 0) return PTTH_GET;
   if(strcmp(method, "PUT") == 0) return PTTH_PUT;
-  if(strcmp(method, "POST") == 0) return PTTH_PUT;
+  if(strcmp(method, "POST") == 0) return PTTH_POST;
   if(strcmp(method, "PATCH") == 0) return PTTH_PATCH;
   return PTTH_DELETE;
 }
@@ -182,32 +320,85 @@ char *ptth_get_req_endpoint(char *req) {
   return ret;
 }
 
-void ptth_handle(int cfd, ptth_services_t *services[5]) {
-  printf("[SERVER] ");
+void ptth_log(enum ptth_methods_e method, char *ed, int fine) {
+  
+  char tbuffer[100];
+  struct tm* tm_info;
+  time_t timer;
+  
+  timer = time(NULL);
+  tm_info = localtime(&timer);
+  strftime(tbuffer, 100, "%Y-%m-%d %H:%M:%S ", tm_info);
 
-  char request[REQUEST_BUFFER_SIZE];
-  int bytes_read = read(cfd, request, REQUEST_BUFFER_SIZE-1);
+  write(1, tbuffer, strlen(tbuffer));
+
+  if(method == PTTH_GET) write(1, "[GET   ]", 8);
+  if(method == PTTH_POST) write(1, "[POST  ]", 8);
+  if(method == PTTH_PUT) write(1, "[PUT   ]", 8);
+  if(method == PTTH_PATCH) write(1, "[PATCH ]", 8);
+  if(method == PTTH_DELETE) write(1, "[DELETE]", 8);
+  
+  if(fine) {
+    write(1, " [FINE   ]", 10);
+  } else {
+    write(1, " [INVALID]", 10);
+  }
+
+  write(1, " ", 1);
+  write(1, ed, strlen(ed));
+  write(1, "\n", 1);
+}
+
+void ptth_handle(int cfd, ptth_services_t *services[5]) {
+  char request[PTTH_REQ_BUFFER_SIZE];
+  int bytes_read = read(cfd, request, PTTH_REQ_BUFFER_SIZE-1);
   
   ptth_request_t req;
   ptth_shared_t *sh;
 
   char *req_ep = ptth_get_req_endpoint(request);
-  printf("%s\n", req_ep);
+  char *sreq_ep = malloc(strlen(req_ep));
+  strcpy(sreq_ep, req_ep);
 
   ptth_response_t resp;
-  ptth_service_t *head = services[ptth_get_method(request)] -> head;
-
+  enum ptth_methods_e meth = ptth_get_method(request);
+  ptth_service_t *head = services[meth] -> head;
+  
   int fine = 0;
+  
+  char *hold_split_endpoint[PTTH_SIZE_OF_MAP];
 
   while(head != NULL) {
-    if(strncmp(head -> endpoint, req_ep, strlen(head->endpoint)) == 0) {
-      resp = head -> sproc(req, sh);
+    if(regexec(head -> endpoint, req_ep, 0, NULL, 0) == 0) {
+      ssize_t oc = 0, sc = -1, cc = 0;
+      char *token = strtok(sreq_ep, "/");
+      size_t ec = 0;
+      while(token != NULL) {
+        hold_split_endpoint[ec] = token;
+        token = strtok(NULL, "/");
+        ec++;
+      }
+
+      for(size_t i = 0; i < head -> url_map -> len; i++) {
+        ptth_map_set_value(head -> url_map, head -> url_map -> keys[i], hold_split_endpoint[head -> order[i]]);
+      }
+      
+      req.mpath =  head -> url_map;
       fine = 1;
+      ptth_log(meth, req_ep, fine);
+      resp = head -> sproc(req, sh);
       break;
     }
+    head = head -> next;
   }
-
-  if(fine == 0) close(cfd);
+  
+  
+  if(fine == 0) {
+    ptth_log(meth, req_ep, fine);
+    write(cfd, PTTH1_1_404_NOT_FOUND, sizeof(PTTH1_1_404_NOT_FOUND));
+    close(cfd);
+    return;
+  }
 
   write(cfd, resp.headers.head, strlen(resp.headers.head));
   write(cfd, resp.body.body, strlen(resp.body.body));
